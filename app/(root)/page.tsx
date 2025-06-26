578e622a14cde1e6f0a99475ba7c4eaa5d1ee218
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-06-26 22:00:23 +0545

    Add utility functions, button component, and update layout with theme support and setup the basic project structure and prefered theme

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-screen">
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Home;
