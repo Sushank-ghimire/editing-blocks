c21fe6fef82d208a20ff78975fc32db5f87f9caf
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-04 13:15:06 +0545

    Add icon constants for various UI elements

---
+++ constants/icons.ts
@@ -0,0 +1,57 @@
+import AddIcon from "@/public/icons/add.svg";
+import ArrowClockwise from "@/public/icons/arrow-clockwise.svg";
+import ArrowCounterClockWise from "@/public/icons/arrow-counterclockwise.svg";
+import Bell from "@/public/icons/bell.svg";
+import Close from "@/public/icons/close.svg";
+import Comment from "@/public/icons/comment.svg";
+import DeleteModel from "@/public/icons/delete-modal.svg";
+import Delete from "@/public/icons/delete.svg";
+import Edit from "@/public/icons/edit.svg";
+import Document from "@/public/icons/doc.svg";
+import File from "@/public/icons/file.svg";
+import h1 from "@/public/icons/h1.svg";
+import h2 from "@/public/icons/h2.svg";
+import h3 from "@/public/icons/h3.svg";
+import JournalText from "@/public/icons/journal-text.svg";
+import Justify from "@/public/icons/justify.svg";
+import Loader from "@/public/icons/loader.svg";
+import More from "@/public/icons/more.svg";
+import Share from "@/public/icons/share.svg";
+import TextCenter from "@/public/icons/text-center.svg";
+import TextLeft from "@/public/icons/text-left.svg";
+import TextParagraph from "@/public/icons/text-paragraph.svg";
+import TextRight from "@/public/icons/text-right.svg";
+import Bold from "@/public/icons/type-bold.svg";
+import Italic from "@/public/icons/type-italic.svg";
+import StrikeThrough from "@/public/icons/type-strikethrough.svg";
+import Underline from "@/public/icons/type-underline.svg";
+
+export {
+  AddIcon,
+  ArrowClockwise,
+  ArrowCounterClockWise,
+  Bell,
+  Close,
+  Comment,
+  DeleteModel,
+  Delete,
+  Edit,
+  Document,
+  File,
+  h1,
+  h2,
+  h3,
+  JournalText,
+  Justify,
+  Loader,
+  More,
+  Share,
+  TextCenter,
+  TextLeft,
+  TextParagraph,
+  TextRight,
+  Bold,
+  Italic,
+  StrikeThrough,
+  Underline,
+};
