9e6f97c4089552f2909717c40649b8d5db7a862b
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-04 13:42:20 +0545

    Enhance document editor with image and mathematics support, and update layout

--- app/(root)/documents/[slug]/editor/page.tsx
+++ app/(root)/documents/[slug]/editor/page.tsx
@@ -5,24 +5,31 @@ import Subscript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
+import Image from "@tiptap/extension-image";
 import Heading from "@tiptap/extension-heading";
 import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import { Dropcursor } from "@tiptap/extensions";
+import Underline from "@tiptap/extension-underline";
+import Mathematics from "@tiptap/extension-mathematics";
+import EditorToolbar from "@/components/editor/Toolbar";
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Underline,
       Subscript,
       Superscript,
       TaskItem.configure({
         nested: true,
       }),
+      Mathematics,
       TaskList,
       Heading.configure({
-        levels: [1, 2, 3, 4],
+        levels: [1, 2, 3],
       }),
       Table.configure({
         resizable: true,
@@ -30,23 +37,16 @@ const Tiptap = () => {
       TableHeader,
       TableCell,
       TableRow,
+      Image.configure({
+        inline: true,
+      }),
+      Dropcursor,
     ],
     content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
-      `,
+      <h1>
+        This editor supports <span data-type="inline-math" data-latex="\\LaTeX"></span> math expressions. And it even supports converting old $\\sub(3*5=15)$ calculations.
+      </h1>
+    `,
     editorProps: {
       attributes: {
         class:
@@ -56,7 +56,8 @@ const Tiptap = () => {
   });
 
   return (
-    <div className="w-full flex items-center justify-center px-4 py-6">
+    <div className="w-full flex flex-col gap-2 items-center justify-center px-4 py-6">
+      <EditorToolbar editor={editor} />
       <div className="w-full max-w-screen-lg rounded-md border border-input bg-card text-card-foreground shadow-md overflow-hidden">
         <div className="border-b border-border px-4 py-2 bg-muted text-muted-foreground text-sm font-medium">
           Start editing Your Document
