af0ec6d3e8e982c9f398a728750152579ce514d6
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-04 10:12:19 +0545

    Update document editor with table content and add theme toggle button

--- app/(root)/documents/[slug]/editor/page.tsx
+++ app/(root)/documents/[slug]/editor/page.tsx
@@ -31,7 +31,22 @@ const Tiptap = () => {
       TableCell,
       TableRow,
     ],
-    content: "<p>Start editing your document...</p>",
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     editorProps: {
       attributes: {
         class:
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
+  const { setTheme } = useTheme();
+
+  const handleThemeChange = () => {
+    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
+  };
   return (
     <div className="flex justify-center flex-col gap-4 items-center min-h-screen w-screen">
-      <Button>Button</Button>
+      <Button onClick={handleThemeChange}>Change Theme</Button>
       <Link href={"/sign-up"}>Signup</Link>
       <Link href={"/documents/document-id-goes-here"}>Documents</Link>
     </div>
