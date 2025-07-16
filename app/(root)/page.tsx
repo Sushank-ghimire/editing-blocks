798cf1d62dfae6a4888356a1a44739de58bb9c0e
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-16 18:53:01 +0545

    setup the real-time database with convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useQuery } from "convex/react";
 import Navbar from "./_components/Navbar";
 import TemplatesGallary from "./_components/template-gallary";
+import { api } from "@/convex/_generated/api";
 
 const Homepage = () => {
+  const documents = useQuery(api.documents.getDocuments);
   return (
     <main className="min-h-screen w-screen">
       <div className="w-full flex h-fit items-center">
